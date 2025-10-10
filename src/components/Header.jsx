d6e8d010c5afa98feae8e48dfd12d425026eebc9
Polina Zherikova bc2025-3 <polinagerikova@gmail.com>
2025-10-11 01:20:21 +0300

    Create Header component with navigation links

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li>Головна</li>
+          <li>Аналіз</li>
+          <li>Список витрат</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
